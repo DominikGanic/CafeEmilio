8cdec2b744dd945842b12652f0e505312d01f03b
Dominik Ganic <dominik@leetlabs.de>
2025-06-15 12:23:25 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+
+// Styles
+import "./assets/css/main.css";
+
+// App Entry Point
+import App from "./App.vue";
+
+// Create and mount the Vue application
+createApp(App).mount("#app");
